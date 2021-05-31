c92e84461d9e1c3389379a42a8cc8e251ca131f9
dattphan15 <ken.phan@live.ca>
2021-05-30 18:02:06 -0700

    filter product by name

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 import products from './products';
 
 const productName: string = "fanny pack";
+
+const product = products.filter(product => product.name === productName)[0];
+
+console.log(product);
