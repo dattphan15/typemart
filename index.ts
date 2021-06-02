492a68b60d6c4331a05d1c3b01d24a1883db74c9
dattphan15 <ken.phan@live.ca>
2021-06-02 12:12:28 -0700

    add conditional if statement for free shipping

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import products from './products';
 
-const productName: string = "fanny pack";
+const productName: string = "shirt";
 const preSale: string = "true";
 let shipping: number;
 let taxPercent: number;
@@ -9,12 +9,19 @@ let total: number;
 const shippingAddress: string = '575 Broadway, New York City, New York';
 
 const product = products.filter(product => {
-
   if (product.name === productName) { 
-      if (product.preOrder === preSale) {
+    if (product.preOrder === preSale) { 
       console.log("Thank you for your pre-order! We'll let you know as soon as we ship your items.")
-  }
-      return product;
+    }
+    if (Number(product.price) >= 25) {
+      shipping = 0;
+      console.log("You qualify for free shipping.")
+    }
+    if (Number(product.price) < 25) {
+      shipping = 5;
+      console.log("Shipping is $5")
+    }
+    return product;
   }
 
 })[0];
