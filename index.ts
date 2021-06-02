2c98fff5213e0950433ecbc3a44a44f3bf7bf04b
dattphan15 <ken.phan@live.ca>
2021-06-02 11:54:42 -0700

    adds conditonal if statement products with pre order

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
 import products from './products';
 
 const productName: string = "fanny pack";
+const preSale: string = "true";
 
-const product = products.filter(product => product.name === productName)[0];
+const product = products.filter(product => {
+
+  if (product.name === productName) { 
+      if (product.preOrder === preSale) {
+      console.log("Thank you for your pre-order! We'll let you know as soon as we ship your items.")
+  }
+      return product;
+  }
+
+})[0];
 
 console.log(product);
